f25315783ff2568cee947ca39edb5aea1975e9ac
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2025-05-12 12:02:16 +0200

    Update User.java

--- InventoryMgtSystem/src/main/java/com/jackmwisadev/InventoryMgtSystem/models/User.java
+++ InventoryMgtSystem/src/main/java/com/jackmwisadev/InventoryMgtSystem/models/User.java
@@ -21,16 +21,23 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @NotBlank(message = "Name is required")
     private String name;
+
     @NotBlank(message = "Email is required")
     private String email;
+
     @NotBlank(message = "Password is required")
     private String password;
+
     @NotBlank(message = "Phone Number is required")
     private String phoneNumber;
+
     private UserRole role;
+
     private List<Transaction> transactions;
+
     private LocalDateTime createdAt = LocalDateTime.now();
 
 
