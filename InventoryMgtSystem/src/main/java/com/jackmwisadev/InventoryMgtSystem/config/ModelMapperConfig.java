14d6589d6012687c4fd6633e2d08e29601aa4a60
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2025-05-12 12:01:33 +0200

    model

---
+++ InventoryMgtSystem/src/main/java/com/jackmwisadev/InventoryMgtSystem/config/ModelMapperConfig.java
@@ -0,0 +1,20 @@
+package com.jackmwisadev.InventoryMgtSystem.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper(){
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setMethodAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setMatchingStrategy(MatchingStrategies.STANDARD);
+        return modelMapper;
+    }
+}
---
+++ InventoryMgtSystem/src/main/java/com/jackmwisadev/InventoryMgtSystem/models/User.java
@@ -0,0 +1,39 @@
+package com.jackmwisadev.InventoryMgtSystem.models;
+
+import jakarta.persistence.*;
+import jakarta.transaction.Transaction;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+@Data
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "Name is required")
+    private String name;
+    @NotBlank(message = "Email is required")
+    private String email;
+    @NotBlank(message = "Password is required")
+    private String password;
+    @NotBlank(message = "Phone Number is required")
+    private String phoneNumber;
+    private UserRole role;
+    private List<Transaction> transactions;
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+
+
+
+}
