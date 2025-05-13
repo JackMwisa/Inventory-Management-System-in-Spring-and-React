db08abc75d337e6791ad37cfc552f9493af20bd1
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2025-05-13 10:26:41 +0200

    Update Response.java

--- InventoryMgtSystem/src/main/java/com/jackmwisadev/InventoryMgtSystem/dtos/Response.java
+++ InventoryMgtSystem/src/main/java/com/jackmwisadev/InventoryMgtSystem/dtos/Response.java
@@ -1,13 +1,33 @@
 package com.jackmwisadev.InventoryMgtSystem.dtos;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.jackmwisadev.InventoryMgtSystem.enums.UserRole;
 import lombok.Builder;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Response {
-    
+
+    //Generic
+    private int status;
+    private String message;
+    //For Login
+    private String token;
+    private UserRole role;
+    private String expirationTime;
+
+
+    //For pagination
+    private Integer totalPages;
+    private  Long totalElement;
+
+    //data output optionals
+    private  UserDTO userDTO;
+    private List<UserDTO> users;
+
 
 }
