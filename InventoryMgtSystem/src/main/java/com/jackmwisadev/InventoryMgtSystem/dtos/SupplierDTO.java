c952976c6c3fe860db2e270284fe386392a34fde
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2025-05-13 10:05:14 +0200

    transaction DTO

--- InventoryMgtSystem/src/main/java/com/jackmwisadev/InventoryMgtSystem/dtos/SupplierDTO.java
+++ InventoryMgtSystem/src/main/java/com/jackmwisadev/InventoryMgtSystem/dtos/SupplierDTO.java
@@ -1,5 +1,7 @@
 package com.jackmwisadev.InventoryMgtSystem.dtos;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -7,15 +9,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "suppliers")
-@Data
-@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SupplierDTO {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
 
     @NotBlank(message = "Name is required")
--- InventoryMgtSystem/src/main/java/com/jackmwisadev/InventoryMgtSystem/dtos/TransactionDTO.java
+++ InventoryMgtSystem/src/main/java/com/jackmwisadev/InventoryMgtSystem/dtos/TransactionDTO.java
@@ -1,5 +1,7 @@
 package com.jackmwisadev.InventoryMgtSystem.dtos;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.jackmwisadev.InventoryMgtSystem.enums.TransactionStatus;
 import com.jackmwisadev.InventoryMgtSystem.enums.TransactionType;
 import jakarta.persistence.*;
@@ -11,25 +13,23 @@ import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-@Entity
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "transactions")
-@Data
-@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TransactionDTO {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
 
     private Integer totalProducts;
 
     private BigDecimal totalPrice;
 
-    @Enumerated(EnumType.STRING)
+
     private TransactionType transactionType; // sale, returned,..
 
-    @Enumerated(EnumType.STRING)
+
     private TransactionStatus transactionStatus; // pending, completes,...
 
     private String description;
@@ -37,30 +37,13 @@ public class TransactionDTO {
     private final LocalDateTime createdAt = LocalDateTime.now();
     private LocalDateTime updateAt;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "product_id")
     private ProductDTO product;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+
     private UserDTO user;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "supplier_id")
+
     private SupplierDTO supplier;
 
-    @Override
-    public String toString() {
-        return "Transaction{" +
-                "id=" + id +
-                ", totalProducts=" + totalProducts +
-                ", totalPrice=" + totalPrice +
-                ", transactionType=" + transactionType +
-                ", transactionStatus=" + transactionStatus +
-                ", description='" + description + '\'' +
-                ", note='" + note + '\'' +
-                ", createdAt=" + createdAt +
-                ", updateAt=" + updateAt +
-                '}';
-    }
+
 }
